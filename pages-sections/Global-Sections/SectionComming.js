59f1de66592907c984ebbd1b19ffd7dba3b07b16
bonomoon <moonbonoz@gmail.com>
2020-03-30 06:25:22 +0900

    update: comming section
    - 문의 및 링크란 추가

--- pages-sections/Global-Sections/SectionComming.js
+++ pages-sections/Global-Sections/SectionComming.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
 import Footer from "components/Footer/Footer.js";
 
 import background from "assets/img/global/comming-bg.jpg";
@@ -32,11 +33,35 @@ export default function SectionComming() {
               <h2 className={classes.title}>
                 <strong>서비스 구현 전</strong>입니다.
               </h2>
-              <h5>
-                &nbsp;AM:PM DevOps와 함께 AM:PM 공식 웹 서비스 구현(기획,
-                디자인, 개발)에 참여하고 싶으시다면 아래의 연락처와 링크를
-                참고해주십시오.
+              <h5 className={classes.description}>
+                &nbsp;<strong>AM:PM DevOps</strong>와 함께 AM:PM 공식 웹 서비스
+                구현(기획, 디자인, 개발)에 참여하고 싶으시다면 아래의 연락처와
+                링크를 참고해주십시오.
               </h5>
+              <div>
+                <Button
+                  className={classes.textButton}
+                  href={
+                    "mailto:ampm.jbnu.315@gmail.com?Subject=AM:PM%20DevOps%20문의"
+                  }
+                  target="_blank"
+                  style={{ color: "#2997ff" }}
+                  simple
+                >
+                  <u>메일 문의하기&nbsp;&nbsp;&nbsp;❯</u>
+                </Button>
+              </div>
+              <div>
+                <Button
+                  className={classes.textButton}
+                  href={"https://github.com/ampm-jbnu/ampm-site"}
+                  target="_blank"
+                  style={{ color: "#2997ff" }}
+                  simple
+                >
+                  <u>깃허브 구경하기 &nbsp;&nbsp;&nbsp;❯</u>
+                </Button>
+              </div>
             </div>
           </GridItem>
           <GridItem xs={12} sm={12} md={3} lg={3}>
@@ -48,7 +73,7 @@ export default function SectionComming() {
           </GridItem>
         </GridContainer>
       </div>
-      <Footer whiteFont containerStyle={{backgroundColor: "transparent"}}/>
+      <Footer whiteFont containerStyle={{ backgroundColor: "transparent" }} />
     </div>
   );
 }
