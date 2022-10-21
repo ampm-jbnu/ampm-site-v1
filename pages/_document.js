37443715ccc1b75aefd81bef469503252d4863fc
bonomoon <bongohmoon@gmail.com>
2022-10-21 09:07:53 +0000

    fix: resolve missing document components especially Html component

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/styles";
 
 class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta
@@ -35,7 +35,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
